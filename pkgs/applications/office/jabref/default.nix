a51c90a60cf3786933765f825581ad33757d8bc8
Gabriel Ebner <gebner@gebner.org>
2017-02-22 15:23:21 +0100

    wip

--- pkgs/applications/office/jabref/default.nix
+++ pkgs/applications/office/jabref/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, fetchurl, makeWrapper, makeDesktopItem, ant, jdk, jre }:
+{ stdenv, fetchurl, makeWrapper, makeDesktopItem, ant, jdk, jre,
+  wrapGAppsHook, glib, gnome3 }:
 
 stdenv.mkDerivation rec {
-  version = "3.8.1";
+  version = "3.8.2";
   name = "jabref-${version}";
 
   src = fetchurl {
     url = "https://github.com/JabRef/jabref/releases/download/v${version}/JabRef-${version}.jar";
-    sha256 = "11asfym74zdq46i217z5n6vc79gylcx8xn7nvwacfqmym0bz79cg";
+    sha256 = "02kjg61l0hx2r87fnbvz4h6n6yndkfg1yjww2wnyz5jgnsg6w8pp";
   };
 
   desktopItem = makeDesktopItem {
@@ -19,7 +20,8 @@ stdenv.mkDerivation rec {
     exec = "jabref";
   };
 
-  buildInputs = [ makeWrapper jdk ];
+  buildInputs = [ makeWrapper jdk
+    wrapGAppsHook glib gnome3.gsettings_desktop_schemas ];
 
   phases = [ "installPhase" ];
 
@@ -34,6 +36,8 @@ stdenv.mkDerivation rec {
     ln -s $src $out/share/java/jabref-${version}.jar
     makeWrapper ${jre}/bin/java $out/bin/jabref \
       --add-flags "-jar $out/share/java/jabref-${version}.jar"
+
+    wrapGAppsHook
   '';
 
   meta = with stdenv.lib; {
